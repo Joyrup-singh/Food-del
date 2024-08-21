7fae043d71802f409bb145e2a8f37b91dbc27cef
Joyrup Singh <dixit1165@gmail.com>
2024-08-21 09:24:39 +0530

    added the appdownload component

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="header-content">
             <h2>Order your favourite food here</h2>
             <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem debitis animi fugit laboriosam minus cupiditate architecto perspiciatis possimus eius veniam.
+              Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise.Our mission is to satisfy your cravings and elevate your dining experience,one delicious meal at a time
             </p>
             <button>View Menu</button>
         </div>
